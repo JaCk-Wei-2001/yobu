4383815ae0c91fc48d5b90d29063d0786271e8e8
DESKTOP-G5GACR8\Ian <u2b58825@gmail.com>
2025-06-12 10:36:06 +0800

    basic setup

---
+++ src/main/java/com/example/yobu/YobuApplication.java
@@ -0,0 +1,13 @@
+package com.example.yobu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YobuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YobuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yobu/YobuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yobu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YobuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
